11d6c6a1c3c4d46f5ee07aa604b7abb83c1be86a
artem090587 <0994210902a@gmail.com>
2019-02-14 20:51:56 +0200

    Include header.html
    Вынес хедер в отдельный файл

--- blog/views.py
+++ blog/views.py
@@ -10,8 +10,10 @@ from django.views.generic import ListView, DetailView
 #     if request.method == 'GET':
 #         return render(request, 'blog/index.html', {})
 
-class PostsListView(ListView): # представление в виде списка
-    model = Post                   # модель для представления 
 
-class PostDetailView(DetailView): # детализированное представление модели
+class PostsListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
     model = Post
