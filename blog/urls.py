a18810dbc51ab6193839ac8214a25fa4af291049
artem090587 <0994210902a@gmail.com>
2019-02-18 21:05:34 +0200

    Create form "add_comment_to_post"
    define add_comment_to_post in views.py
    add url pattern in urls.py
    create add_comment_to_post.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.urls import path
+from . import views
 from blog.views import PostsListView, PostDetailView
 
 
@@ -11,4 +12,5 @@ urlpatterns = [
     # url(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='post-detail'),
     # path('<int:id>/', PostDetailView.as_view(), name='post-detail'),
     path('<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from blog.models import Post 
 from django.views.generic import ListView, DetailView
 from blog.models import Category
+from .forms import CommentForm
+from django.shortcuts import redirect
 
 # def index(request):
 #     if request.method == 'GET':
@@ -19,3 +21,17 @@ class PostDetailView(DetailView):
     def get_object(self, queryset=None):
         slug_ = self.kwargs.get("slug")
         return get_object_or_404(Post, slug=slug_)
+
+
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
