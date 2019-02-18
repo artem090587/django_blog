28c53c31585cc7f79a827f50fe6f910cc324f158
artem090587 <0994210902a@gmail.com>
2019-02-18 21:25:13 +0200

    define Approove Remoove methods
    in views & urls

--- blog/urls.py
+++ blog/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     # path('<int:id>/', PostDetailView.as_view(), name='post-detail'),
     path('<slug:slug>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from blog.models import Post 
+from blog.models import Post, Comment 
 from django.views.generic import ListView, DetailView
 from blog.models import Category
 from .forms import CommentForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 
 # def index(request):
 #     if request.method == 'GET':
@@ -35,3 +36,16 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
